c1271f00f76f8bc4f7f24ed644c7138ec48d4214
pikpik <pikpik.1010@gmail.com>
2012-10-06 19:30:34 -0400

    Some header files were extra.

--- source/main.cpp
+++ source/main.cpp
@@ -1,15 +1,7 @@
 #include <iostream>
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 
-#include "network.h"
 #include "session.h"
 
 
